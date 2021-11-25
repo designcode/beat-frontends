f28ea2f05b36b3f3cbc4ae7af76f3e51fc1259cb
A Ibrahim <abdullah.ibrahim@zivver.com>
2021-11-25 17:17:25 +0100

    refactor(webapp): Moves global styles to main.scss

--- apps/webapp/src/main.tsx
+++ apps/webapp/src/main.tsx
@@ -3,29 +3,9 @@ import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import styled from '@emotion/styled';
 import App from './app/app';
+import './main.scss';
 
 const StyledApp = styled.div`
-  font-family: Arial;
-  font-size: 1rem;
-
-  * {
-    margin: 0;
-    padding: 0;
-    color: #333;
-    line-height: 1.5rem;
-  }
-
-  a {
-    text-decoration: none;
-  }
-
-  h1 {
-    font-size: 2rem;
-  }
-
-  h1, h2, h3, h4, h5 {
-    margin: 0 0 2rem 0;
-  }
 `;
 
 ReactDOM.render(
