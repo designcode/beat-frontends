72748faa966d0c6ce976d303cc9ce7971c1d0424
A Ibrahim <abdullah.ibrahim@zivver.com>
2021-11-26 11:07:22 +0100

    feat(utils): Implements debounce method

--- libs/shared/utils/src/lib/shared-utils.ts
+++ libs/shared/utils/src/lib/shared-utils.ts
@@ -6,4 +6,19 @@ export const formateDate = (date: Date): string =>
     day: 'numeric',
   });
 
-export const formatCurrency = (amount: string) => `$ ${amount.replace('S/ ', '')}`;
+export const formatCurrency = (amount: string) =>
+  `$ ${amount.replace('S/ ', '')}`;
+
+export const debounce = <F extends (...args: unknown[]) => unknown>(func: F, waitFor: number) => {
+  let timeout: ReturnType<typeof setTimeout> | null = null;
+
+  const debounced = (...args: Parameters<F>) => {
+    if (timeout !== null) {
+      clearTimeout(timeout);
+      timeout = null;
+    }
+    timeout = setTimeout(() => func(...args), waitFor);
+  };
+
+  return debounced as (...args: Parameters<F>) => ReturnType<F>;
+};
